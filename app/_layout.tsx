de0f1af0feb0cdb4ca254faf3ba7512efaedf70a
terrancrypt <thaiphamngoctuong@gmail.com>
2025-04-22 21:53:38 +0700

    Refactor layout and remove unused requireInit file

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,14 +1,12 @@
-import "@/utils/requireInit";
+import "@openfort/react-native/polyfills";
 import { Slot } from "expo-router";
 import { OpenfortProvider } from "@/hooks/useOpenfort";
 
 export default function RootLayout() {
   return (
-    <>
-      <OpenfortProvider customUri={process.env.EXPO_PUBLIC_IFRAME_URL}>
-        {/* <Redirect href="/main" /> */}
-        <Slot />
-      </OpenfortProvider>
-    </>
+    <OpenfortProvider customUri={process.env.EXPO_PUBLIC_IFRAME_URL}>
+      {/* <Redirect href="/main" /> */}
+      <Slot />
+    </OpenfortProvider>
   );
 }
--- utils/requireInit.ts
+++
@@ -1,7 +0,0 @@
-// // Import react-native-get-random-values first - this is critical
-// import 'react-native-get-random-values';
-// // Then import other crypto-related libraries
-// import 'react-native-randombytes';
-import '@openfort/react-native/polyfills';
-
-// This file doesn't export anything, it just ensures polyfills are loaded in the correct order
